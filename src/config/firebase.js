8e11857f9172a4ec949159cc9276c88ba88b684d
Soufiane <elvetta26@gmail.com>
2026-07-06 11:30:41 +0200

    ADD Firebase Auth

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -10,6 +10,24 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID
 };
 
+/*
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB2sKGrSdg4dhSbpBUdxv0Mt9AL-Z0hAZs",
+
+  authDomain: "mitumi-ae188.firebaseapp.com",
+
+  projectId: "mitumi-ae188",
+
+  storageBucket: "mitumi-ae188.firebasestorage.app",
+
+  messagingSenderId: "834348395535",
+
+  appId: "1:834348395535:web:ec4eabf75dc90899d7c77a"
+
+};
+*/
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
