7a306b84670271e8a228cfada9b2ac100b025546
David Santamaría Rogado <howl.nsp@gmail.com>
2026-07-13 21:53:06 +0200

    add defered fetch example
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/components/TodosLosEspacios.jsx
+++ src/components/TodosLosEspacios.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
 
 export const TodosLosEspacios = () => {
   const API_URL = import.meta.env.VITE_API_URL;
 
+  // Creamos la variable que cuando sea true se realiza el fetch.
+  const [shouldSend, setShouldSend] = useState(false);
+
   const {
     data,
     loading,
     error,
-  } = useFetch(`${API_URL}/espacios`);
+  } = useFetch(shouldSend ? `${API_URL}/espacios` : null);
+  // La url al useFetch se pasa cuando shouldSend se vuelve true.
 
   const espacios = data?.data || [];
 
   if (loading) return <p>Cargando espacios...</p>;
   if (error) return <p>Error al cargar los espacios: {error}</p>;
 
+  // Poner shouldSend a false al dar al botón Hola
+  const loadHandle = () => {
+    setShouldSend(true);
+  };
+
   return (
     <section>
       <h2>Todos los espacios</h2>
+
+      {/* El botón Hola. */}
+      <button onClick={loadHandle}>Hola</button>
 
       <div>
         {espacios.map((espacio) => (
