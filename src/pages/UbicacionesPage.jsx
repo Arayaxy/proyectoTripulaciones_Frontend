c78841139689238987dfef93e2e7da9d79c361b1
Arayax <inigoibarluceamoreno@gmail.com>
2026-07-13 16:17:41 +0200

    actualizado

---
+++ src/pages/UbicacionesPage.jsx
@@ -0,0 +1,26 @@
+import CrearUbicacion from "../components/CrearUbicacion"
+
+export const UbicacionesPage = () => {
+  return (
+    <>
+      <section>
+<CrearUbicacion/>
+      </section>
+      <section>
+        <form action="">
+          {/* formulario crear ubicaccion  */}
+        </form>
+        <button>Crear sala</button>
+        <form>
+          {/* formulario crear sala  */}
+        </form>
+      </section>
+      <section>
+        {/* contenedor ia */}
+      </section>
+      <section>
+        {/* pintar todas las ubicacion */}
+      </section>
+    </>
+  )
+}
