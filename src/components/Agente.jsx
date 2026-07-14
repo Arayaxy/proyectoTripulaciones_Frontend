93bb88eb627c0b27f1975e9d7d302984be0ccf1f
carlospmendiola <carlos@carlospmendiola.com>
2026-07-14 14:04:35 +0200

    Solucionar nombre estilos en Agente y OpcionesAgente

--- src/components/Agente.jsx
+++ src/components/Agente.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './partials/_agentes.scss'
+import './partials/_agente.scss'
 
 export const Agente = () => {
   return (
     <>
 
       Esta es la página de agentes
-   
+
     </>
   )
 }
