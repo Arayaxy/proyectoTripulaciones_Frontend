615b0ccca9666c1ff0314a8844e7c92a4dc5042e
Arayax <inigoibarluceamoreno@gmail.com>
2026-07-14 12:39:58 +0200

    !actualizar navBar
    Signed-off-by: Arayax <inigoibarluceamoreno@gmail.com>

--- src/components/EventoInfo.jsx
+++ src/components/EventoInfo.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+
 import './partials/_eventos.scss'
-import { NavbarInterno } from './avbarInterno'
+import { NavbarInterno } from './NavbarInterno'
 
 export const EventoInfo = () => {
   return (
     <>
+    <NavbarInterno/>
       Este es el componente de Info Evento
     </>
   )
