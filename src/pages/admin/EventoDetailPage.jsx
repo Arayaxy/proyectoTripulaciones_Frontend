b01a05657bdbbd9ed3af6181a14cb6c40936076b
esti2127 <elarrinagalopez@gmail.com>
2026-07-15 09:17:44 +0200

    componente eventoInfo

--- src/pages/admin/EventoDetailPage.jsx
+++ src/pages/admin/EventoDetailPage.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { NavbarInterno } from '../../components/NavbarInterno'
 import { EventoInfo } from '../../components/EventoInfo'
 import { EventoDetail } from '../../components/EventoDetail'
+import { useParams } from 'react-router'
 
 export const EventoDetailPage = () => {
+  const { id } = useParams()
+
   return (
     <>
       <header className='titlePage'>
@@ -11,7 +14,7 @@ export const EventoDetailPage = () => {
       </header>
       <section className='container'>
         <NavbarInterno />
-        <EventoInfo />
+        <EventoInfo eventId={id} />
         <EventoDetail />
       </section>
     </>
